8200a863ced28096c740de24697320c2d908e556
y2k <itwisterlx@gmail.com>
2026-09-23 20:28:14 +0200

    Add macOS alias for showing local IP address

--- macos/zshrc.zsh
+++ macos/zshrc.zsh
@@ -2,6 +2,7 @@ plugins+=(macos)
 
 alias mac_gc="find . -type f \( -name '.DS_Store' -o -name '._*' \) -delete"
 alias update_ai="brew update && brew upgrade claude-code@latest opencode-v2 openspec"
+alias ip='ipconfig getifaddr "$(route -n get default | awk '\''/interface:/ {print $2}'\'')"'
 
 export COPYFILE_DISABLE=1
 
