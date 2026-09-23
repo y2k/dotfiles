ed849b6b0573cbcf4c42a7e3da450f4fe7bed781
y2k <itwisterlx@gmail.com>
2026-09-23 20:52:04 +0200

    Use Homebrew Android SDK and add Superpowers plugin config

--- macos/android.zsh
+++ macos/android.zsh
@@ -1,4 +1,4 @@
-export ANDROID_HOME="${ANDROID_HOME:-$HOME/Library/Android/sdk}"
+export ANDROID_HOME="${ANDROID_HOME:-/opt/homebrew/share/android-commandlinetools}"
 
 if [[ -d "$ANDROID_HOME" ]]; then
   path=("$ANDROID_HOME/platform-tools" "$ANDROID_HOME/cmdline-tools/latest/bin" $path)
